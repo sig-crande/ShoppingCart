7da93f5709dcd44f8c501ce7a657dfa78e81eb4f
Challa Rao Ande <crande@synopsys.com>
2021-12-08 00:33:09 +0530

    add top secret algorithm

---
+++ src/main/java/com/crypto/TopSecretCryptoOperation.java
@@ -0,0 +1,21 @@
+//This is a top secret file
+package com.crypto;
+
+public class TopSecretCryptoOperation {
+
+    private int strength;
+    private String topSecret;
+
+    public TopSecretCryptoOperation(int strength, String topSecret){
+        this.strength = strength;
+        this.topSecret = topSecret;
+    }
+
+    public void doCrypto(){
+        System.out.println("Do the top secret operation now.....");
+        System.out.println("I'm afraid I will mess this up.");
+        System.out.println("Praying I don't print the secret out.");
+        System.out.println("The strength of the operation is: "+ strength);
+        System.out.println("Operation completed.");
+    }
+}
